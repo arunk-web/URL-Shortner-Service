570a8dc1cb26227cd1f74365cab1986f427ebfc3
arunk-web <arunk02615@gmail.com>
2026-06-11 17:39:33 +0530

    fix models path

--- Controller/url.js
+++ Controller/url.js
@@ -1,7 +1,7 @@
 
 const shortid = require("shortid")
-const URL = require('../models/url');
-
+const URL = require('../Models/url');
+ 
 async function handleGenerateNewShortURL(req,res){
     const body = req.body;
     if(!body.url) return res.status(400).json({error : "url is required "})
